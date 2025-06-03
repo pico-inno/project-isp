d51d629b0926e728e196f9b4d77622fb91b806cb
paingsoeko <dev.kopaing@gmail.com>
2025-06-03 23:35:01 +0700

    add radisu migration and model

---
+++ app/Models/NasReload.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NasReload extends Model
+{
+    protected $table = 'nasreload';
+}
---
+++ app/Models/RadPostAuth.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RadPostAuth extends Model
+{
+    protected $table = 'radpostauth';
+}
